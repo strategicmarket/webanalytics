6208bb7b0dd2aac75f178f5b0051dd4a4fd7ff87
pdhoward <patrick.howard@hotmail.com>
2017-10-31 16:05:24 -0400

    updates

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 'use strict';
 /////////////////////////////////////////////////////
-////////  		unit test server               ///////
+////////  		platform management             ///////
 ///////         xio labs v 1.2.0            ///////
 //////////////////////////////////////////////////
-
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -15,6 +14,40 @@ const app = express()
 
 app.use(express.static('public'))
 app.use(cors())
+
+//////////////////////////////////////////////////////////////////
+////////////     authentication fuctions           //////////////
+//////////             auth0 saas                 //////////////
+///////////////////////////////////////////////////////////////
+const jwt = require('express-jwt');
+const jwtAuthz = require('express-jwt-authz');
+const jwksRsa = require('jwks-rsa');
+require('dotenv').config();
+
+if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
+  throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file'
+}
+
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.AUTH0_AUDIENCE,
+  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+  algorithms: ['RS256']
+});
+
+const checkScopes = jwtAuthz([ 'read:messages' ]);
+const checkScopesAdmin = jwtAuthz([ 'write:messages' ]);
+
+/////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
 
 // help doc
 app.get('/', (req, res) => {
