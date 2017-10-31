8411d95b9d4504a69617f7ac8d799bb373cf6f4a
pdhoward <patrick.howard@hotmail.com>
2017-10-31 16:07:08 -0400

    updates

--- server.js
+++ server.js
@@ -84,6 +84,7 @@ app.use((req, res, next) => {
   }
 })
 
+
 // api catalogue
 app.get('/contacts', (req, res) => {
   res.send(contacts.get(req.token))
@@ -104,6 +105,19 @@ app.post('/contacts', bodyParser.json(), (req, res) => {
     })
   }
 })
+
+///////////////test routines auth0 ////////////////////
+app.get('/api/public', function(req, res) {
+  res.json({ message: "Hello from a public endpoint! You don't need to be authenticated to see this." });
+});
+
+app.get('/api/private', checkJwt, checkScopes, function(req, res) {
+  res.json({ message: "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this." });
+});
+
+app.post('/api/admin', checkJwt, checkScopesAdmin, function(req, res) {
+  res.json({ message: "Hello from an admin endpoint! You need to be authenticated and have a scope of write:messages to see this." });
+});
 
 
 // spin up http server
