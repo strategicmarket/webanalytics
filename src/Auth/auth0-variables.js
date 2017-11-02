3e3e1cd1c96a6b26a5a934a1463ad40d25c05c7a
pdhoward <patrick.howard@hotmail.com>
2017-11-02 16:27:52 -0400

    updates

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -10,6 +10,6 @@ export const AUTH_CONFIG = {
 export const AUTH_CONFIG = {
   domain: 'xio.auth0.com',
   clientId: 'lucJJx21Ghg5DIxC8ENnODy92lbsOfge',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'http://localhost:3000/',
   apiUrl: 'http://localhost:3001'
 }
