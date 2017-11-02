c18331437e4e3bba68f9d706379b0abb8352c717
pdhoward <patrick.howard@hotmail.com>
2017-11-02 15:45:07 -0400

    updates

--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,8 +1,18 @@
 import React, {Component} from "react";
 import {Container, Row, Col, CardGroup, Card, CardBlock, Button, Input, InputGroup, InputGroupAddon} from "reactstrap";
 
+// authentication
+import Auth from '../../Auth/Auth';
+import Login from '../../views/Pages/Login/Login'
+
+const auth = new Auth();
 
 class Login extends Component {
+
+  login() {
+      this.props.auth.login();
+    }
+
   render() {
     return (
       <div className="app flex-row align-items-center">
@@ -12,22 +22,14 @@ class Login extends Component {
               <CardGroup className="mb-0">
                 <Card className="p-4">
                   <CardBlock className="card-body">
-                    <h1>Login</h1>
-                    <p className="text-muted">Sign In to your account</p>
-                    <InputGroup className="mb-3">
-                      <InputGroupAddon><i className="icon-user"></i></InputGroupAddon>
-                      <Input type="text" placeholder="Username"/>
-                    </InputGroup>
-                    <InputGroup className="mb-4">
-                      <InputGroupAddon><i className="icon-lock"></i></InputGroupAddon>
-                      <Input type="password" placeholder="Password"/>
-                    </InputGroup>
+                    <h1>Welcome</h1>
+                    <p className="text-muted">To the Conversational Economy</p>
                     <Row>
                       <Col xs="6">
-                        <Button color="primary" className="px-4">Login</Button>
+                        <Button color="primary" className="px-4">Login  </Button>
                       </Col>
-                      <Col xs="6" className="text-right">
-                        <Button color="link" className="px-0">Forgot password?</Button>
+                      <Col xs="6">
+                        <Button color="primary" className="px-4">Message</Button>
                       </Col>
                     </Row>
                   </CardBlock>
@@ -35,10 +37,12 @@ class Login extends Component {
                 <Card className="text-white bg-primary py-5 d-md-down-none" style={{ width: 44 + '%' }}>
                   <CardBlock className="card-body text-center">
                     <div>
-                      <h2>Sign up</h2>
-                      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                        labore et dolore magna aliqua.</p>
-                      <Button color="primary" className="mt-3" active>Register Now!</Button>
+                      <h2>Disrupt</h2>
+                      <p> Conversations managed. With every customer</p>
+                      <p> Brands. Sales. Products. Orders. Pricing. Status. </p>
+                      <p> Business in the conversational economy
+                      </p>
+
                     </div>
                   </CardBlock>
                 </Card>
