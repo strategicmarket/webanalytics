f0c022b09d1e281e1bc9ca058586adbde731573e
pdhoward <patrick.howard@hotmail.com>
2017-11-04 15:19:12 -0400

    updates:

--- src/Auth/Callback/Secure.js
+++ src/Auth/Callback/Secure.js
@@ -1,6 +1,27 @@
-import React from 'react';
-import Callback from './Callback';
+import React, { Component } from 'react';
+import loading from './loading.svg';
 
-const Secure = (props) => <Callback />;
+class Secure extends Component {
+
+  render() {
+    const style = {
+      position: 'absolute',
+      display: 'flex',
+      justifyContent: 'center',
+      height: '100vh',
+      width: '100vw',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: 'white',
+    }
+    return (
+      <div style={style}>
+        <h2>Work in Progress</h2>
+      </div>
+    );
+  }
+}
 
 export default Secure;
